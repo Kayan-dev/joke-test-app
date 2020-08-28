b4bf2ceaa0a91103634cd000f9b23c48139a7562
Kayan-dev <kaya-development@outlook.com>
2020-08-28 10:39:27 +0200

    added the joke

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('./node_modules/express');
+
+const app = express();
+ 
+const port = 3000;
+
+
+
+    // HTML string
+const page =  `<html>
+  <head>
+    <title>Yigit Efecan Kaya</title>
+  </head>
+ <h1>Why do comedians love !false?</h1>
+  <body>
+    <p>It's funny because it's true.</p>
+  </body>
+</html>`
+
+
+// confirmation function
+function onListen() {
+    console.log(`Listening on :${port}`)
+}
+// handler function
+function onRequest() {
+    console.log('testing... request')
+}
+
+app.get(
+    '/', // path
+    (request, response) => { // handler callback
+        const message = `My favorite move is ${request.params.title}`;
+        response.send(page)
+        },
+    
+    
+)
+
+
+
+// start listening
+app.listen(
+    port, // TCP port where the server listens
+    onListen, // callback runs when server starts
+)
